0fd8f282562e84b85ca8bfe481bbc8a7bcbfc798
min-kim123 <107881708+min-kim123@users.noreply.github.com>
2022-11-04 23:29:41 -0700

    Update studentlist.cpp

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,7 +1,7 @@
 /* 
 Author: Min Kim
 Program Description: This program will help you keep track of your students. You can input as many studets as you like, and you will be able to store their first name, last name, id, and gpa.
-Date: 11/04/22
+Date: 11/04/2022
   */
 #include <iostream>
 #include <iomanip>
@@ -17,49 +17,41 @@ struct Student {
 };
 
 
-void add(vector<Student*> & studentlist) {
-/*  struct Student newstruct;
-    cout << "First name: ";
-    cin.getline(newstruct.firstname, 80);
-    cout << "Last name: ";
-    cin.getline(newstruct.lastname, 80);
-    cout << "ID: ";
-    cin >> newstruct.id;
-    cout << "GPA: ";
-    cin >> newstruct.gpa;
-    studentlist.push_back(newstruct);
-  }*/
+void add(vector<Student*> & studentlist) {//adds new student information into studentlist vector
+  Student* newstudent = new Student();
+  char temp_firstname[80];
+  char temp_lastname[80];
+  int temp_id;
+  float temp_gpa;
 
-  Student* addStudent = new Student();
-  char firstNameTemp[30];
-  char lastNameTemp[30];
-  int studentIDTemp;
-  float gpaTemp;
-
-  // inputting everything
+  // get the new student information
+  cout << "------------------------" << endl;
   cout << "First Name: " << endl;
-  cin >> firstNameTemp;
+  cin.getline(temp_firstname, 80);
   cout << "Last Name: " << endl;
-  cin >> lastNameTemp;
+  cin.getline(temp_lastname, 80);
   cout << "ID: " << endl;
-  cin >> studentIDTemp;
+  cin >> temp_id;
   cout << "GPA: " << endl;
-  cin >> gpaTemp;
+  cin >> temp_gpa;
+  cout << "------------------------" << endl;
 
-  // copys from temp variables into the vector
-  strcpy(addStudent->firstname, firstNameTemp);
-  strcpy(addStudent->lastname, lastNameTemp);
-  addStudent->id = studentIDTemp;
-  addStudent->gpa = gpaTemp;
+  // copys the temporary variables into studentlist vector
+  strcpy(newstudent->firstname, temp_firstname);
+  strcpy(newstudent->lastname, temp_lastname);
+  newstudent->id = temp_id;
+  newstudent->gpa = temp_gpa;
 
-  studentlist.push_back(addStudent);
+  studentlist.push_back(newstudent);
 
 }
 
-void delet(vector<Student*> & studentlist) {
+void delet(vector<Student*> & studentlist) {//deletes info of student from specified id
   int deleteID = 0;
+  cout << "------------------------" << endl;
   cout << "Student ID: ";
   cin >> deleteID;
+  cout << "------------------------" << endl;
   int num = 0;
   for (int i = 0; i < studentlist.size(); ++i) {
     if (deleteID == studentlist[i]->id) {
@@ -71,7 +63,7 @@ void delet(vector<Student*> & studentlist) {
 }
 
 
-void print(vector<Student*> & studentlist) {
+void print(vector<Student*> & studentlist) {//prints everything in the studentlist vector
   cout<<fixed<<setprecision(2);
   for (int i = 0; i < studentlist.size(); ++i) {
   cout << "------------------------" << endl;
@@ -88,13 +80,13 @@ void print(vector<Student*> & studentlist) {
 int main() {
   char input[20];
   bool cont = true;
+  //a vector of pointers to studentlist
   vector <Student*> studentlist;
   while (cont == true) {
-    cout << "Would you like to add, print, delete, or quit? (ADD, PRINT, DELETE, QUIT): ";
+    cout << "Would you like to add, print, delete, or quit? (ADD, PRINT, DELETE, QUIT): " << endl;
     cin.getline(input, 20);
     if (strcmp(input, "ADD") == 0) {//add
       add(studentlist);
-      cout << "d";
     }
     else if (strcmp(input, "PRINT") == 0) {//print
       print(studentlist);
